fd8bb574c558c144eb930144f0ae900ca63d5bca
Artem Glotov <glotovartemalex@gmail.com>
2018-11-18 19:19:44 +0300

    Migrated to AndroidX. Added Navigation libraries

--- app/src/main/java/com/filfur/softwaredesign/MainActivity.kt
+++ app/src/main/java/com/filfur/softwaredesign/MainActivity.kt
@@ -6,15 +6,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.support.design.widget.Snackbar
+import com.google.android.material.snackbar.Snackbar
 import android.telephony.TelephonyManager
 import android.widget.Toast
 
